13ae6df5db4df03267f7788c0ee68cf169a0a4c8
rafliyusuf999 <133180576+rafliyusuf999@users.noreply.github.com>
2024-09-20 17:22:55 +0700

    Add files via upload

---
+++ 065002400058-RafliYusufAldani_modul1_Latihan Kedua.py
@@ -0,0 +1,6 @@
+print("hitung luas ruangan:")
+panjang = float(input("panjang: "))
+lebar = float(input("lebar: "))
+satuan = input("cm/m: ")
+A = panjang * lebar
+print("hasilnya adalah:", A, satuan)
---
+++ 065002400058-RafliYusufAldani_modul1_Latihan Satu.py
@@ -0,0 +1,6 @@
+nama = input("nama kamu siapa :")
+asal = input("dari mana kamu :")
+sekolah = input("sebelumnya kamu sekolah dimana ?")
+print ("selamat siang"+nama )
+print ("wah dari "+asal)
+print("pasti menyenangkan sekolah di"+sekolah)
